8be2fa9642a87da7cc935e5db86288a8fdfe3875
Zeke Sikelianos <zeke@sikelianos.com>
2016-11-30 17:13:30 -0800

    stuff

--- bin/build.js
+++ bin/build.js
@@ -1,14 +1,29 @@
-const Report = require('./lib/report')
-const utils = require('./lib/utils')
+const Report = require('../lib/report')
+const utils = require('../lib/utils')
 const {uniq, pick} = require('lodash')
 const repos = require('repos-using-electron').filter(repo => !repo.fork)
 const registry = require('all-the-packages')
 const Package = require('nice-package')
 const electronNpmPackages = require('electron-npm-packages')
-const MAX_RESULTS = 100
+const MAX_RESULTS = 500
 const topDeps = utils.getTopDependencies(repos).slice(0, MAX_RESULTS)
 const topDevDeps = utils.getTopDevDependencies(repos).slice(0, MAX_RESULTS)
 const allPackageNames = uniq(topDeps.map(d => d.name).concat(topDevDeps.map(d => d.name)))
+const packageData = {}
+
+const desirablePackageProps = [
+  'name',
+  'description',
+  // 'dependencies',
+  // 'devDependencies',
+  'dependents',
+  'devDependents',
+  'totalDeps',
+  'downloadsInLastMonth',
+  'repository',
+  'keywords',
+  'author'
+]
 
 registry
   .on('package', function (pkg) {
@@ -20,21 +35,23 @@ registry
   })
   .on('end', finish)
 
+// finish()
+
 function finish () {
   // Add extra package metadata to dep lists
-  topDeps.forEach(dep => Object.assign(dep, packageData[dep.name]) )
-  topDevDeps.forEach(dep => Object.assign(dep, packageData[dep.name]) )
+  topDeps.forEach(dep => Object.assign(dep, packageData[dep.name]))
+  topDevDeps.forEach(dep => Object.assign(dep, packageData[dep.name]))
 
   new Report({
     title: 'Top Dependencies in Apps',
     description: 'npm packages that are used most often as `dependencies` in Electron apps',
-    collection: topDeps
+    collection: topDeps.map(package => pick(package, desirablePackageProps))
   }).save()
 
   new Report({
     title: 'Top Development Dependencies in Apps',
     description: 'npm packages that are used most often as `devDependencies` in Electron apps',
-    collection: topDevDeps
+    collection: topDevDeps.map(package => pick(package, desirablePackageProps))
   }).save()
 
   new Report({
@@ -47,6 +64,8 @@ function finish () {
     title: 'Electron-specific packages most-depended-on by other npm packages',
     description: 'Electron-specific npm packages that are depended on by other npm packages',
     collection: electronNpmPackages
+      .map(package => pick(package, desirablePackageProps))
+      .map(utils.convertDepListToCount)
       .sort((a, b) => b.totalDeps - a.totalDeps)
       .slice(0, MAX_RESULTS)
   }).save()
@@ -55,6 +74,8 @@ function finish () {
     title: 'Most-downloaded Electron-specific npm Packages',
     description: 'Electron-specific npm packages that are depended on by other npm packages',
     collection: electronNpmPackages
+      .map(package => pick(package, desirablePackageProps))
+      .map(utils.convertDepListToCount)
       .sort((a, b) => b.downloadsInLastMonth - a.downloadsInLastMonth)
       .slice(0, MAX_RESULTS)
   }).save()
